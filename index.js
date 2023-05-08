0e508c4bf6ab884d0a09d557e38fb6b7ae48db0d
bhagavath15 <bhagavath.sbkgmail.com>
2023-05-08 08:16:53 +0530

    auth

--- index.js
+++ index.js
@@ -27,14 +27,14 @@ app.use(express.json())
 // Use bodyParser middleware to parse JSON in request body
 app.use(bodyParser.json());
 
-app.get("/", function (request, response) {
+app.get("/", auth, function (request, response) {
 
     response.send("🙋‍♂️, 🌏 🎊✨🤩");
 
 });
 
 
-app.post("/dashboard", async function (request, response) {
+app.post("/dashboard", auth, async function (request, response) {
     const result = request.body
     const detail = await client
         .db("workspace")
@@ -44,7 +44,7 @@ app.post("/dashboard", async function (request, response) {
 
 })
 
-app.get("/dashboard", async function (request, response) {
+app.get("/dashboard", auth, async function (request, response) {
 
     const detail = await client
         .db("workspace")
@@ -55,7 +55,7 @@ app.get("/dashboard", async function (request, response) {
 
 })
 
-app.get("/dashboard/:id", async function (request, response) {
+app.get("/dashboard/:id", auth, async function (request, response) {
     const { id } = request.params
     const detail = await client
         .db("workspace")
@@ -64,7 +64,7 @@ app.get("/dashboard/:id", async function (request, response) {
     detail ? response.send(detail) : response.status(404).send({ message: "Detail is not found" })
 })
 
-app.put("/dashboard/:id", async function (request, response) {
+app.put("/dashboard/:id", auth, async function (request, response) {
     const { id } = request.params
     const data = request.body
     const detail = await client
@@ -74,7 +74,7 @@ app.put("/dashboard/:id", async function (request, response) {
     response.send(detail)
 })
 
-app.delete('/dashboard/:id', async function (request, response) {
+app.delete('/dashboard/:id', auth, async function (request, response) {
     const { id } = request.params
     console.log(id)
 
